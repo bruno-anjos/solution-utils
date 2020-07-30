362b4a08aa75fd69bfd81d077cc41e537fd80397
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-30 17:00:15 +0100

    remove unnecessary error formatting

--- http_utils/replies.go
+++ http_utils/replies.go
@@ -2,29 +2,19 @@ package http_utils
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/pkg/errors"
 )
 
-const (
-	errorSendingJSONReplyFormat = "error replying with JSON: %s"
-)
-
 func SendJSONReplyOK(w http.ResponseWriter, replyContent interface{}) {
-	SendJSONReplyWithStatus(w, http.StatusOK, replyContent)
-}
-
-func SendJSONReplyWithStatus(w http.ResponseWriter, status int, replyContent interface{}) {
 	toSend, err := json.Marshal(replyContent)
 	if err != nil {
-		panic(fmt.Sprintf(errorSendingJSONReplyFormat, errors.WithStack(err)))
+		panic(errors.WithStack(err))
 	}
 
-	w.WriteHeader(status)
 	_, err = w.Write(toSend)
 	if err != nil {
-		panic(fmt.Sprintf(errorSendingJSONReplyFormat, errors.WithStack(err)))
+		panic(errors.WithStack(err))
 	}
 }

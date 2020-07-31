ae5a54594016a230bb56656e3190b854ca08f68a
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-31 16:34:56 +0100

    remove useless error with stack on panic

--- http_utils/replies.go
+++ http_utils/replies.go
@@ -3,18 +3,16 @@ package http_utils
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/pkg/errors"
 )
 
 func SendJSONReplyOK(w http.ResponseWriter, replyContent interface{}) {
 	toSend, err := json.Marshal(replyContent)
 	if err != nil {
-		panic(errors.WithStack(err))
+		panic(err)
 	}
 
 	_, err = w.Write(toSend)
 	if err != nil {
-		panic(errors.WithStack(err))
+		panic(err)
 	}
 }

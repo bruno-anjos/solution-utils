77f8fdea43959b41fa5e737d24f1290c8eec1dd3
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-31 16:35:15 +0100

    complete StartServer helper method

--- servers.go
+++ servers.go
@@ -1,9 +1,10 @@
 package generic_utils
 
 import (
-	"fmt"
+	"flag"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/bruno-anjos/solution-utils/http_utils"
@@ -11,16 +12,33 @@ import (
 )
 
 const (
-	// DefaultHost contains the default interface address
-	DefaultHost = "0.0.0.0"
+	// DefaultInterface contains the default interface address
+	DefaultInterface = "0.0.0.0"
 )
 
 // StartServer seeds the random generator and starts a server on the
-// specified host and port serving the routes passed.
-func StartServer(serviceName, host string, port int, prefixPath string, routes []http_utils.Route) {
+// specified host and port serving the routes passed with a specified prefix.
+func StartServer(serviceName, hostPort string, port int, prefixPath string, routes []http_utils.Route) {
 	rand.Seed(time.Now().UnixNano())
-	addr := fmt.Sprintf("%s:%d", host, port)
+
+	debug := flag.Bool("d", false, "add debug logs")
+	listenAddr := flag.String("l", DefaultInterface, "address to listen on")
+	flag.Parse()
+
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	log.Debug("starting log in debug mode")
 	r := http_utils.NewRouter(prefixPath, routes)
-	log.Infof("starting %s server in port %d...\n", serviceName, port)
-	log.Fatal(http.ListenAndServe(addr, r))
+
+	var listenAddrPort string
+	if *listenAddr != "" {
+		listenAddrPort = *listenAddr + ":" + strconv.Itoa(port)
+	} else {
+		listenAddrPort = hostPort
+	}
+
+	log.Infof("%s server listening at %s...\n", serviceName, listenAddrPort)
+	log.Fatal(http.ListenAndServe(listenAddrPort, r))
 }

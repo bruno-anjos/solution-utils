562c9f14e46cad611b3762e833e299257fef39fe
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-08-03 17:02:06 +0100

    add helper function

---
+++ utils.go
@@ -0,0 +1,18 @@
+package generic_utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	rand.Seed(time.Now().Unix())
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}

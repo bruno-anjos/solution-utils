1b13d1fb2d9bc694beb8ceec3dd2214ada79bdd8
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-08-13 17:19:30 +0100

    change panics for loggings

--- http_utils/requests.go
+++ http_utils/requests.go
@@ -54,13 +54,13 @@ func DoRequest(httpClient *http.Client, request *http.Request, responseBody inte
 
 	resp, err := httpClient.Do(request)
 	if err != nil {
-		panic(err)
+		log.Error(err)
 	}
 
 	if responseBody != nil {
 		err = json.NewDecoder(resp.Body).Decode(responseBody)
 		if err != nil {
-			panic(err)
+			log.Error(err)
 		}
 	}
 
